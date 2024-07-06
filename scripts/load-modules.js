f5b604e3ccd97f11b02086c1c35e8787899b0d11
VasilyeuUV <vasilyeu.uv@mail.ru>
2024-07-06 11:27:20 +0300

    feat: add header module

---
+++ scripts/load-modules.js
@@ -0,0 +1,7 @@
+const loadModule = async (id, url) => {
+  const response = await fetch(url);
+  const text = await response.text();
+  document.getElementById(id).innerHTML = text;
+};
+
+loadModule('header-container', 'modules/header.html');
